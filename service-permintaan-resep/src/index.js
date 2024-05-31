00ec874f21735cfaac9af6a14653ee58b8f5a182
Aerossky <riskyradeonrx560@gmail.com>
2024-06-01 00:41:48 +0700

    service permintaan resep
    sudah menggabungkan service permintaan resep ke dalam docker

--- service-permintaan-resep/src/index.js
+++ service-permintaan-resep/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const { PrismaClient } = require('@prisma/client');
+const {
+    PrismaClient
+} = require('@prisma/client');
 
 
 dotenv.config();
@@ -10,9 +12,12 @@ const PORT = process.env.PORT;
 // supaya bisa pake prisma
 const prisma = new PrismaClient();
 
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World");
+});
 
-app.get("/permintaans", async (req, res)=>{
-    const permintaans = await prisma.komentar.findMany();
+app.get("/permintaans", async (req, res) => {
+    const permintaans = await prisma.permintaan.findMany();
 
     res.status(200).send(permintaans);
 });
